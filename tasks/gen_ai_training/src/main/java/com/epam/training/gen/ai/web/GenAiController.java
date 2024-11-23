091e16782aed1ac5fe9ffb4e79ef1c90d12cf4ff
Oleh Tkachuk <oleh_tkachuk@epam.com>
2024-11-23 19:41:14 +0100

    Add service interface

--- tasks/gen_ai_training/src/main/java/com/epam/training/gen/ai/web/GenAiController.java
+++ tasks/gen_ai_training/src/main/java/com/epam/training/gen/ai/web/GenAiController.java
@@ -2,6 +2,7 @@ package com.epam.training.gen.ai.web;
 
 import com.epam.training.gen.ai.model.Prompt;
 import com.epam.training.gen.ai.model.Reply;
+import com.epam.training.gen.ai.service.ConversationService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,9 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GenAiController {
 
+    private final ConversationService conversationService;
+
+    public GenAiController(ConversationService conversationService) {
+        this.conversationService = conversationService;
+    }
+
     @PostMapping("/conversations")
     public Reply processPrompt(@RequestBody Prompt prompt) {
-        return new Reply("hi");
+        return new Reply(conversationService.reply(prompt.input()));
     }
 
 }
+
--- tasks/gen_ai_training/src/test/java/com/epam/training/gen/ai/web/GenAiControllerTest.java
+++ tasks/gen_ai_training/src/test/java/com/epam/training/gen/ai/web/GenAiControllerTest.java
@@ -1,30 +1,41 @@
 package com.epam.training.gen.ai.web;
 
+import com.epam.training.gen.ai.service.ConversationService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doReturn;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@ExtendWith(SpringExtension.class)
 @WebMvcTest(GenAiController.class)
 class GenAiControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private ConversationService conversationService;
+
     @Test
     void processPromptTest() throws Exception {
+        doReturn("Hi!").when(conversationService).reply(anyString());
         mockMvc.perform(
                         post("/conversations")
                                 .contentType(MediaType.APPLICATION_JSON)
                                 .content("{\"input\": \"hello there\"}")
                 )
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.text", equalTo("hi")));
+                .andExpect(jsonPath("$.text", equalTo("Hi!")));
     }
 }
