95c31679fe03aa6edb71e602cd9ff3ef433e392e
Oleh Tkachuk <oleh_tkachuk@epam.com>
2024-12-07 22:30:17 +0100

    Document observations

--- tasks/gen_ai_training/src/main/java/com/epam/training/gen/ai/web/ConversationController.java
+++ tasks/gen_ai_training/src/main/java/com/epam/training/gen/ai/web/ConversationController.java
@@ -12,6 +12,18 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
+/**
+ *
+ * - Compare results for the same prompts using different models
+ *      From my observations, each model produces different result for the same prompt.
+ *      Smaller models perform slower and produce a bit worse result in terms of quality,
+ *      while large models give better quality and a quite fast.
+ *      </p>
+ * - Compared results using different PromptExecutionSettings
+ *      The most noticeable effect is from the temperature setting. Values that are closer to 1.0 give
+ *      more 'creative' result, while the values close to 0 produce more focused and concise response
+ * */
+
 @RestController
 @RequestMapping("conversations")
 @RequiredArgsConstructor
